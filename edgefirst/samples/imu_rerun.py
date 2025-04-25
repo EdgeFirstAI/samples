53519fdba7b85f10792759ce8abf6ac5c15074a1
Matt Quigley <matt@au-zone.com>
2025-04-25 15:50:54 -0600

    imu rerun added and updates for gps rerun

--- edgefirst/samples/imu_rerun.py
+++ edgefirst/samples/imu_rerun.py
@@ -1,21 +1,21 @@
-import atexit
-import sys
 import zenoh
 from time import time
 from argparse import ArgumentParser
-from edgefirst.schemas.sensor_msgs import NavSatFix
+from edgefirst.schemas.sensor_msgs import Imu
 import rerun as rr
+from rerun.datatypes import Quaternion
 
 if __name__ == "__main__":
-    args = ArgumentParser(description="EdgeFirst Samples - GPS")
+    args = ArgumentParser(description="EdgeFirst Samples - IMU")
     args.add_argument('-c', '--connect', type=str, default=None,
                       help="Connect to a Zenoh router rather than peer mode.")
     args.add_argument('-t', '--time', type=float, default=None,
                       help="Time in seconds to run command before exiting.")
     args = args.parse_args()
 
-    rr.init("GPS Example", spawn=True)
-    # rr.serve_web_viewer(open_browser=False, web_port=4321)
+    rr.init("IMU Example", spawn=True)
+    box = rr.Boxes3D(half_sizes=[0.5, 0.5, 0.5], fill_mode=rr.components.FillMode.Solid)
+    rr.log("box", box, rr.Transform3D(clear=False, axis_length=2),)
 
     # Create the default Zenoh configuration and if the connect argument is
     # provided set the mode to client and add the target to the endpoints.
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     session = zenoh.open(config)
 
     # Create a subscriber for "rt/imu"
-    subscriber = session.declare_subscriber('rt/gps')
+    subscriber = session.declare_subscriber('rt/imu')
 
     # Keep a list of discovered topics to avoid noise from duplicates
     start = time()
@@ -37,8 +37,13 @@ if __name__ == "__main__":
         msg = subscriber.recv()
 
         # deserialize message
-        gps = NavSatFix.deserialize(msg.payload.to_bytes())
-        lat = gps.latitude
-        long = gps.longitude
-        print("Latitude: %.6f Longitude: %.6f" % (lat, long))
-        rr.log("CurrentLoc", rr.GeoPoints(lat_lon=[lat, long]))
+        imu = Imu.deserialize(msg.payload.to_bytes())
+        x = imu.orientation.x
+        y = imu.orientation.y
+        z = imu.orientation.z
+        w = imu.orientation.w
+        # print("X: %.4f Y: %.4f Z: %.4f W: %.4f" % (x, y, z, w))
+        rr.log("box", rr.Transform3D(clear=False, quaternion=Quaternion(xyzw=[x,y,z,w])))
+
+
+        

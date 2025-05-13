9eef95c5d9eed160e9fc259b6331b8397542dded
Charlie-Zheng <charlie@au-zone.com>
2025-05-13 14:43:04 -0600

    EDGEAI-634 adjusted rust examples locations. Added some fusion python examples

--- python/fusion/boxes3d.py
+++ python/fusion/boxes3d.py
@@ -5,47 +5,46 @@ from argparse import ArgumentParser
 import time
 import atexit
 import sys
+import rerun as rr
 
 
 def handler(sample):
     # Deserialize message
     detect = Detect.deserialize(sample.payload.to_bytes())
-    print(f"Received message: {detect}")
 
 
 def main():
     args = ArgumentParser(description="EdgeFirst Samples - Boxes3D")
-    args.add_argument('-c', '--connect', type=str, default=None,
-                      help="Connect to a Zenoh router rather than peer mode.")
+    args.add_argument('-r', '--remote', type=str, default=None,
+                      help="Connect to a Zenoh router rather than local.")
     args.add_argument('-t', '--timeout', type=float, default=None,
                       help="Time in seconds to run command before exiting.")
+    rr.script_add_args(args)
     args = args.parse_args()
+
+    rr.script_setup(args, "fusion/boxes3d Example")
 
     # Create the default Zenoh configuration and if the connect argument is
     # provided set the mode to client and add the target to the endpoints.
     config = zenoh.Config()
-    if args.connect is not None:
+    if args.remote is not None:
         config.insert_json5("mode", "'client'")
-        config.insert_json5("connect", '{"endpoints": ["%s"]}' % args.connect)
+        config.insert_json5("connect", '{"endpoints": ["%s"]}' % args.remote)
     session = zenoh.open(config)
 
     # Create a subscriber for "rt/fusion/boxes3d"
-    subscriber = session.declare_subscriber('rt/fusion/boxes3d', handler)
+    subscriber = session.declare_subscriber('rt/fusion/boxes3d')
 
-    def _on_exit():
-        session.close()
-    atexit.register(_on_exit)
+    while True:
+        msg = subscriber.recv()
+        detection = Detect.deserialize(msg.payload.to_bytes())
+        print(f"Recieved {len(detection.boxes)} 3D boxes.")
+        centers = [(x.distance, x.center_x, x.center_y)
+                   for x in detection.boxes]
+        sizes = [(x.width, x.width, x.height)
+                 for x in detection.boxes]
 
-    # The declare_subscriber runs asynchronously, so we need to block the main
-    # thread to keep the program running.  We use time.sleep() to do this
-    # but an application could have its main control loop here instead.
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        session.close()
-        sys.exit(0)
+        rr.log("fusion/boxes3d", rr.Boxes3D(centers=centers, sizes=sizes))
 
 
 if __name__ == "__main__":
--- python/fusion/lidar.py
+++ python/fusion/lidar.py
@@ -1,50 +1,49 @@
 import zenoh
 from edgefirst.schemas.sensor_msgs import PointCloud2
+from edgefirst.schemas import decode_pcd, colormap, turbo_colormap
 from argparse import ArgumentParser
-import time
-import atexit
 import sys
-
-
-def handler(sample):
-    # Deserialize message
-    target = PointCloud2.deserialize(sample.payload.to_bytes())
-    print(f"Received message: {target}")
+import rerun as rr
 
 
 def main():
+    """
+    This demo requires lidar output to be enabled on `fusion` to work.
+    By default the rt/fusion/lidar output is not enabled for `fusion`.
+    To enable it, configure LIDAR_OUTPUT_TOPIC="rt/fusion/lidar" to set 
+    command line argument --lidar-output-topic=rt/fusion/lidar
+    """
     args = ArgumentParser(description="EdgeFirst Samples - Lidar")
-    args.add_argument('-c', '--connect', type=str, default=None,
-                      help="Connect to a Zenoh router rather than peer mode.")
+    args.add_argument('-r', '--remote', type=str, default=None,
+                      help="Connect to a Zenoh router rather than local.")
     args.add_argument('-t', '--timeout', type=float, default=None,
                       help="Time in seconds to run command before exiting.")
+    rr.script_add_args(args)
     args = args.parse_args()
+
+    rr.script_setup(args, "fusion/lidar Example")
 
     # Create the default Zenoh configuration and if the connect argument is
     # provided set the mode to client and add the target to the endpoints.
     config = zenoh.Config()
-    if args.connect is not None:
+    if args.remote is not None:
         config.insert_json5("mode", "'client'")
-        config.insert_json5("connect", '{"endpoints": ["%s"]}' % args.connect)
+        config.insert_json5("connect", '{"endpoints": ["%s"]}' % args.remote)
     session = zenoh.open(config)
 
     # Create a subscriber for "rt/fusion/lidar"
-    subscriber = session.declare_subscriber('rt/fusion/lidar', handler)
+    subscriber = session.declare_subscriber('rt/fusion/lidar')
 
-    def _on_exit():
-        session.close()
-    atexit.register(_on_exit)
+    while True:
+        msg = subscriber.recv()
 
-    # The declare_subscriber runs asynchronously, so we need to block the main
-    # thread to keep the program running.  We use time.sleep() to do this
-    # but an application could have its main control loop here instead.
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        session.close()
-        sys.exit(0)
+        pcd = PointCloud2.deserialize(msg.payload.to_bytes())
+        points = decode_pcd(pcd)
+        max_class = max(max([p.fields["vision_class"] for p in points]), 1)
+        pos = [[p.x, p.y, p.z] for p in points]
+        colors = [
+            colormap(turbo_colormap, p.fields["vision_class"]/max_class) for p in points]
+        rr.log("fusion/lidar", rr.Points3D(positions=pos, colors=colors))
 
 
 if __name__ == "__main__":

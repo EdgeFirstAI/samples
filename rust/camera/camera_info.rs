1c0f8b9fefdc26444424a39efaf1873dab0401d0
Matt Quigley <matt@au-zone.com>
2025-05-15 15:50:16 -0600

    further rust examples

---
+++ rust/camera/camera_info.rs
@@ -0,0 +1,32 @@
+use clap::Parser as _;
+use edgefirst_samples::Args;
+use edgefirst_schemas::sensor_msgs::CameraInfo;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let session = zenoh::open(args.clone()).await.unwrap();
+
+    // Create a subscriber for "rt/camera/info"
+    let subscriber = session.declare_subscriber("rt/camera/info").await.unwrap();
+
+    // Create Rerun logger using the provided parameters
+    let (rr, _serve_guard) = args.rerun.init("camera-info")?;
+
+    while let Ok(msg) = subscriber.recv() {
+        let info: CameraInfo = cdr::deserialize(&msg.payload().to_bytes())?;
+
+        let width = info.width;
+        let height = info.height;
+        let _distortion_model = info.distortion_model;
+        let _d = info.d; // Distortion parameters
+        let _k = info.k; // Intrinsic camera matrix
+        let _r = info.r; // Rectification matrix
+        let _p = info.p; // Projection matrix
+        let text = "Camera Width: ".to_owned() + &width.to_string() + " Camera Height: " + &height.to_string();
+        let _ = rr.log("CameraInfo", &rerun::TextLog::new(text));
+    }
+
+    Ok(())
+}

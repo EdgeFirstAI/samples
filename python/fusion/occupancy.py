34162826e10f46392ed4ed8ef27001d90af462ce
saksham <saksham@au-zone.com>
2025-05-28 16:09:37 -0600

    Fixed issues with crashing when no radar points are present

--- python/fusion/occupancy.py
+++ python/fusion/occupancy.py
@@ -34,6 +34,9 @@ def main():
 
         pcd = PointCloud2.deserialize(msg.payload.to_bytes())
         points = decode_pcd(pcd)
+        if not points:
+            rr.log("fusion/occupancy", rr.Points3D(positions=[], colors=[])) 
+            continue
         max_class = max(max([p.fields["vision_class"] for p in points]), 1)
         pos = [[p.x, p.y, p.z] for p in points]
         colors = [
--- python/fusion/radar.py
+++ python/fusion/radar.py
@@ -34,6 +34,8 @@ def main():
 
         pcd = PointCloud2.deserialize(msg.payload.to_bytes())
         points = decode_pcd(pcd)
+        if not points:
+            rr.log("fusion/radar", rr.Points3D(positions=[], colors=[])) 
         max_class = max(max([p.fields["vision_class"] for p in points]), 1)
         pos = [[p.x, p.y, p.z] for p in points]
         colors = [

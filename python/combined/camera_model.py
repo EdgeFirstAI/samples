10d821ea4e0f0f3b0d7487a98f104cd70300a094
Matt Quigley <matt@au-zone.com>
2025-06-10 14:31:30 -0600

    better method for handling messages in python

--- python/combined/camera_model.py
+++ python/combined/camera_model.py
@@ -1,9 +1,11 @@
 import zenoh
 from edgefirst.schemas.edgefirst_msgs import Detect, Mask
 import rerun as rr
+import rerun.blueprint as rrb
 from argparse import ArgumentParser
 import sys
 import av
+import asyncio
 import io
 import time
 import zstd
@@ -13,76 +15,129 @@ import cv2
 raw_data = io.BytesIO()
 container = av.open(raw_data, format='h264', mode='r')
 frame_size = []
+last_h264_msg = None
+last_boxes2d_msg = None
+last_mask_msg = None
 
 def h264_callback(msg):
+    global last_h264_msg
+    last_h264_msg = msg
+
+async def h264_processing():
+    global last_h264_msg
     global frame_size
-    raw_data.write(msg.payload.to_bytes())
-    raw_data.seek(0)
-    for packet in container.demux():
-        try:
-            if packet.size == 0:  # Skip empty packets
-                continue
-            raw_data.seek(0)
-            raw_data.truncate(0)
-            for frame in packet.decode():  # Decode video frames
-                frame_array = frame.to_ndarray(format='rgb24')  # Convert frame to numpy array
-                frame_size = [frame_array.shape[1], frame_array.shape[0]]
-                rr.log('camera', rr.Image(frame_array))
-        except Exception:  # Handle exceptions
-            continue  # Continue processing next packets
+    while True:
+        if last_h264_msg is None:
+            time.sleep(0.001)
+            continue
+
+        raw_data.write(last_h264_msg.payload.to_bytes())
+        raw_data.seek(0)
+        for packet in container.demux():
+            try:
+                if packet.size == 0:  # Skip empty packets
+                    continue
+                raw_data.seek(0)
+                raw_data.truncate(0)
+                for frame in packet.decode():  # Decode video frames
+                    frame_array = frame.to_ndarray(format='rgb24')  # Convert frame to numpy array
+                    frame_size = [frame_array.shape[1], frame_array.shape[0]]
+                    rr.log('camera', rr.Image(frame_array))
+            except Exception:  # Handle exceptions
+                continue  # Continue processing next packets
+        last_h264_msg = None
 
 def boxes2d_callback(msg):
-    if len(frame_size) != 2:
-        return
-    centers = []
-    sizes = []
-    labels = []
+    global last_boxes2d_msg
+    last_boxes2d_msg = msg
 
-    detection = Detect.deserialize(msg.payload.to_bytes())    
-    for box in detection.boxes:
-        centers.append((int(box.center_x * frame_size[0]), int(box.center_y * frame_size[1])))
-        sizes.append((int(box.width * frame_size[0]), int(box.height * frame_size[1])))
-        labels.append(box.label)
-    rr.log("camera/boxes", rr.Boxes2D(centers=centers, sizes=sizes, labels=labels))
+async def boxes2d_processing():
+    global last_boxes2d_msg
+    while True:
+        if last_boxes2d_msg is None:
+            time.sleep(0.001)
+            continue
+
+        if len(frame_size) != 2:
+            return
+        centers = []
+        sizes = []
+        labels = []
+
+        detection = Detect.deserialize(last_boxes2d_msg.payload.to_bytes())    
+        for box in detection.boxes:
+            centers.append((int(box.center_x * frame_size[0]), int(box.center_y * frame_size[1])))
+            sizes.append((int(box.width * frame_size[0]), int(box.height * frame_size[1])))
+            labels.append(box.label)
+        rr.log("camera/boxes", rr.Boxes2D(centers=centers, sizes=sizes, labels=labels))
+        last_boxes2d_msg = None
 
 def mask_callback(msg):
-    if len(frame_size) != 2:
-        return
-    mask = Mask.deserialize(msg.payload.to_bytes())
-    decoded_array = zstd.decompress(bytes(mask.mask))
-    np_arr = np.frombuffer(decoded_array, np.uint8)
-    np_arr = np.reshape(np_arr, [mask.height, mask.width, -1])
-    np_arr = cv2.resize(np_arr, frame_size)
-    np_arr = np.argmax(np_arr, axis=2)
-    rr.log("/", rr.AnnotationContext([(0, "background", (0,0,0,0)), (1, "person", (0,255,0))]))
-    rr.log("camera/mask", rr.SegmentationImage(np_arr))
+    global last_mask_msg
+    last_mask_msg = msg
 
-def main():
-    args = ArgumentParser(description="EdgeFirst Samples - CameraModel")
-    args.add_argument('-r', '--remote', type=str, default=None,
-                      help="Connect to a Zenoh router rather than local.")
-    rr.script_add_args(args)
-    args = args.parse_args()
+async def mask_processing():
+    global last_mask_msg
+    while True:
+        if last_mask_msg is None:
+            time.sleep(0.001)
+            continue
 
+        if len(frame_size) != 2:
+            return
+        mask = Mask.deserialize(last_mask_msg.payload.to_bytes())
+        decoded_array = zstd.decompress(bytes(mask.mask))
+        np_arr = np.frombuffer(decoded_array, np.uint8)
+        np_arr = np.reshape(np_arr, [mask.height, mask.width, -1])
+        np_arr = cv2.resize(np_arr, frame_size)
+        np_arr = np.argmax(np_arr, axis=2)
+        rr.log("/", rr.AnnotationContext([(0, "background", (0,0,0,0)), (1, "person", (0,255,0))]))
+        rr.log("camera/mask", rr.SegmentationImage(np_arr))
+        last_mask_msg = None
+
+async def main_async(args):
+    # Setup rerun
+    args.memory_limit = 10
     rr.script_setup(args, "camera_model")
 
-    # Create the default Zenoh configuration and if the connect argument is
-    # provided set the mode to client and add the target to the endpoints.
+    blueprint = rrb.Blueprint(
+        rrb.Grid(contents=[
+            rrb.Spatial2DView(origin="/camera", name="Camera Feed"),
+        ])
+    )
+    rr.send_blueprint(blueprint)
+
+    # Zenoh config
     config = zenoh.Config()
     config.insert_json5("scouting/multicast/interface", "'lo'")
-    if args.remote is not None:
+    if args.remote:
         config.insert_json5("mode", "'client'")
-        config.insert_json5("connect", '{"endpoints": ["%s"]}' % args.remote)
+        config.insert_json5("connect", f'{{"endpoints": ["{args.remote}"]}}')
     session = zenoh.open(config)
 
-    # Create the necessary subscribers
-    subscriber1 = session.declare_subscriber('rt/camera/h264', h264_callback)
-    subscriber2 = session.declare_subscriber('rt/model/boxes2d', boxes2d_callback)
-    # Mask callback is currently disabled due to desync caused by cv2 resize
-    # subscriber3 = session.declare_subscriber('rt/model/mask_compressed', mask_callback)
+    # Declare subscribers
+    session.declare_subscriber('rt/camera/h264', h264_callback)
+    session.declare_subscriber('rt/model/boxes2d', boxes2d_callback)
+    session.declare_subscriber('rt/model/mask_compressed', mask_callback)
 
-    while True:
-        time.sleep(0.1)
+    # Launch concurrent processing tasks
+    await asyncio.gather(
+        h264_processing(),
+        boxes2d_processing(),
+        mask_processing()
+    )
+
+def main():
+    parser = ArgumentParser(description="EdgeFirst Samples - Camera Model")
+    parser.add_argument('-r', '--remote', type=str, default=None,
+                        help="Connect to the remote endpoint instead of local.")
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+
+    try:
+        asyncio.run(main_async(args))
+    except KeyboardInterrupt:
+        sys.exit(0)
         
 
 if __name__ == "__main__":    
--- python/combined/camera_radar.py
+++ python/combined/camera_radar.py
@@ -1,77 +1,107 @@
 from argparse import ArgumentParser
-import time
-import rerun as rr
-import rerun.blueprint as rrb
+import asyncio
 import io
+import sys
 import av
 import zenoh
+import rerun as rr
+import rerun.blueprint as rrb
+from edgefirst.schemas.edgefirst_msgs import Detect
+from edgefirst.schemas.sensor_msgs import PointCloud2
+from edgefirst.schemas import decode_pcd, colormap, turbo_colormap
 
 raw_data = io.BytesIO()
 container = av.open(raw_data, format='h264', mode='r')
 frame_size = []
+last_h264_msg = None
+last_boxes2d_msg = None
+last_radar_msg = None
 
 def h264_callback(msg):
-    global frame_size
+    global last_h264_msg
+    last_h264_msg = msg
 
-    raw_data.write(msg.payload.to_bytes())
-    raw_data.seek(0)
-    for packet in container.demux():
-        try:
-            if packet.size == 0:  # Skip empty packets
+async def h264_processing():
+    global last_h264_msg, frame_size
+    while True:
+        if last_h264_msg is None:
+            await asyncio.sleep(0.001)
+            continue
+
+        raw_data.write(last_h264_msg.payload.to_bytes())
+        raw_data.seek(0)
+        for packet in container.demux():
+            try:
+                if packet.size == 0:
+                    continue
+                raw_data.seek(0)
+                raw_data.truncate(0)
+                for frame in packet.decode():
+                    frame_array = frame.to_ndarray(format='rgb24')
+                    frame_size = [frame_array.shape[1], frame_array.shape[0]]
+                    rr.log('camera', rr.Image(frame_array))
+            except Exception:
                 continue
-            raw_data.seek(0)
-            raw_data.truncate(0)
-            for frame in packet.decode():  # Decode video frames
-                frame_array = frame.to_ndarray(format='rgb24')  # Convert frame to numpy array
-                frame_size = [frame_array.shape[1], frame_array.shape[0]]
-                rr.log('/camera', rr.Image(frame_array))
-        except Exception:  # Handle exceptions
-            continue  # Continue processing next packets
+        last_h264_msg = None
+        await asyncio.sleep(0)  # Yield control
 
 def boxes2d_callback(msg):
-    from edgefirst.schemas.edgefirst_msgs import Detect
-    if len(frame_size) != 2:
-        return
-    centers = []
-    sizes = []
-    labels = []
+    global last_boxes2d_msg
+    last_boxes2d_msg = msg
 
-    detection = Detect.deserialize(msg.payload.to_bytes())    
-    for box in detection.boxes:
-        centers.append((int(box.center_x * frame_size[0]), int(box.center_y * frame_size[1])))
-        sizes.append((int(box.width * frame_size[0]), int(box.height * frame_size[1])))
-        labels.append(box.label)
-    rr.log("/camera/boxes", rr.Boxes2D(centers=centers, sizes=sizes, labels=labels))
+async def boxes2d_processing():
+    global last_boxes2d_msg, frame_size
+    while True:
+        if last_boxes2d_msg is None:
+            await asyncio.sleep(0.001)
+            continue
+
+        if len(frame_size) != 2:
+            await asyncio.sleep(0.001)
+            continue
+
+        centers, sizes, labels = [], [], []
+
+        detection = Detect.deserialize(last_boxes2d_msg.payload.to_bytes())
+        for box in detection.boxes:
+            centers.append((int(box.center_x * frame_size[0]), int(box.center_y * frame_size[1])))
+            sizes.append((int(box.width * frame_size[0]), int(box.height * frame_size[1])))
+            labels.append(box.label)
+        rr.log("camera/boxes", rr.Boxes2D(centers=centers, sizes=sizes, labels=labels))
+        last_boxes2d_msg = None
+        await asyncio.sleep(0)
 
 def radar_clusters_callback(msg):
-    from edgefirst.schemas.sensor_msgs import PointCloud2
-    from edgefirst.schemas import decode_pcd, colormap, turbo_colormap
-    pcd = PointCloud2.deserialize(msg.payload.to_bytes())
-    points = decode_pcd(pcd)
-    clusters = [p for p in points if p.id > 0]
-    max_id = max(max([p.id for p in clusters]), 1)
-    pos = [[p.x, p.y, p.z] for p in clusters]
-    colors = [colormap(turbo_colormap, p.id/max_id) for p in clusters]
-    rr.log("/pointcloud/radar/clusters", rr.Points3D(pos, colors=colors))
+    global last_radar_msg
+    last_radar_msg = msg
 
-def main():
-    args = ArgumentParser(description="EdgeFirst Samples - Camera Radar")
-    args.add_argument('-r', '--remote', type=str, default=None,
-                      help="Connect to the remote endpoint instead of local.")
-    rr.script_add_args(args)
-    args = args.parse_args()
+async def radar_processing():
+    global last_radar_msg
+    while True:
+        if last_radar_msg is None:
+            await asyncio.sleep(0.001)
+            continue
 
-    # Create the default Zenoh configuration and if the remote argument is
-    # provided set the mode to client and add the target to the endpoints.
-    config = zenoh.Config()
-    config.insert_json5("scouting/multicast/interface", "'lo'")
-    if args.remote is not None:
-        config.insert_json5("mode", "'client'")
-        config.insert_json5("connect", '{"endpoints": ["%s"]}' % args.remote)
-    session = zenoh.open(config)
+        pcd = PointCloud2.deserialize(last_radar_msg.payload.to_bytes())
+        points = decode_pcd(pcd)
+        clusters = [p for p in points if p.id > 0]
+        if not clusters:
+            last_radar_msg = None
+            await asyncio.sleep(0)
+            continue
 
-    args.memory_limit=10
-    rr.script_setup(args, "mega_sample")
+        max_id = max(p.id for p in clusters)
+        pos = [[p.x, p.y, p.z] for p in clusters]
+        colors = [colormap(turbo_colormap, p.id / max_id) for p in clusters]
+        rr.log("/pointcloud/radar/clusters", rr.Points3D(pos, colors=colors))
+        last_radar_msg = None
+        await asyncio.sleep(0)
+
+async def main_async(args):
+    # Setup rerun
+    args.memory_limit = 10
+    rr.script_setup(args, "camera_radar")
+
     blueprint = rrb.Blueprint(
         rrb.Grid(contents=[
             rrb.Spatial2DView(origin="/camera", name="Camera Feed"),
@@ -80,26 +110,37 @@ def main():
     )
     rr.send_blueprint(blueprint)
 
-    cam_subscriber = session.declare_subscriber('rt/camera/h264', h264_callback)
-    boxes2d_subscriber = session.declare_subscriber('rt/model/boxes2d', boxes2d_callback)
-    radar_clusters_subscriber = session.declare_subscriber('rt/radar/clusters', radar_clusters_callback)
+    # Zenoh config
+    config = zenoh.Config()
+    config.insert_json5("scouting/multicast/interface", "'lo'")
+    if args.remote:
+        config.insert_json5("mode", "'client'")
+        config.insert_json5("connect", f'{{"endpoints": ["{args.remote}"]}}')
+    session = zenoh.open(config)
 
-    while True:
-        try:
-            time.sleep(0.1)
-        except KeyboardInterrupt:
-            if cam_subscriber:
-                cam_subscriber.undeclare()
-            if boxes2d_subscriber:
-                boxes2d_subscriber.undeclare()
-            if radar_clusters_subscriber:
-                radar_clusters_subscriber.undeclare()
-            rr.disconnect()
-            break
-            
+    # Declare subscribers
+    session.declare_subscriber('rt/camera/h264', h264_callback)
+    session.declare_subscriber('rt/model/boxes2d', boxes2d_callback)
+    session.declare_subscriber('rt/radar/clusters', radar_clusters_callback)
 
-if __name__ == "__main__":    
+    # Launch concurrent processing tasks
+    await asyncio.gather(
+        h264_processing(),
+        boxes2d_processing(),
+        radar_processing()
+    )
+
+def main():
+    parser = ArgumentParser(description="EdgeFirst Samples - Camera Radar")
+    parser.add_argument('-r', '--remote', type=str, default=None,
+                        help="Connect to the remote endpoint instead of local.")
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+
+    try:
+        asyncio.run(main_async(args))
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+if __name__ == "__main__":
     main()
-
-
-    
